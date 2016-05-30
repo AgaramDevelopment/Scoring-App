68016b881ea022b1f4cf25bf731c2da339950370
AgaramDevelopment <apple@apples-macbook-pro-2.local>
2016-05-30 15:42:38 +0530

    Ball Event Type changed

--- CAPScoringApp/CAPScoringApp/Resource/BallEvents/BallEventRecord.h
+++ CAPScoringApp/CAPScoringApp/Resource/BallEvents/BallEventRecord.h
@@ -18,19 +18,19 @@
 
 @property(nonatomic,strong) NSString * objTeamcode;
 
-@property(nonatomic,strong) NSString * objDayno;
+@property(nonatomic,strong) NSNumber * objDayno;
 
-@property(nonatomic,strong) NSString * objInningsno;
+@property(nonatomic,strong) NSNumber * objInningsno;
 
-@property(nonatomic,strong) NSString * objOverno;
+@property(nonatomic,strong) NSNumber * objOverno;
 
-@property(nonatomic,strong) NSString * objBallno;
+@property(nonatomic,strong) NSNumber * objBallno;
 
-@property(nonatomic,strong) NSString * objBallcount;
+@property(nonatomic,strong) NSNumber * objBallcount;
 
-@property(nonatomic,strong) NSString * objOverBallcount;
+@property(nonatomic,strong) NSNumber * objOverBallcount;
 
-@property(nonatomic,strong) NSString * objSessionno;
+@property(nonatomic,strong) NSNumber * objSessionno;
 
 @property(nonatomic,strong) NSString * objStrikercode;
 
@@ -52,71 +52,71 @@
 
 @property(nonatomic,strong) NSString * objShottype;
 
-@property(nonatomic,strong) NSString * objIslegalball;
+@property(nonatomic,strong) NSNumber * objIslegalball;
 
-@property(nonatomic,strong) NSString * objIsFour;
+@property(nonatomic,strong) NSNumber * objIsFour;
 
-@property(nonatomic,strong) NSString * objIssix;
+@property(nonatomic,strong) NSNumber * objIssix;
 
-@property(nonatomic,strong) NSString * objRuns;
+@property(nonatomic,strong) NSNumber * objRuns;
 
-@property(nonatomic,strong) NSString * objOverthrow;
+@property(nonatomic,strong) NSNumber * objOverthrow;
 
-@property(nonatomic,strong) NSString * objTotalruns;
+@property(nonatomic,strong) NSNumber * objTotalruns;
 
-@property(nonatomic,strong) NSString * objWide;
+@property(nonatomic,strong) NSNumber * objWide;
 
-@property(nonatomic,strong) NSString * objNoball;
+@property(nonatomic,strong) NSNumber * objNoball;
 
-@property(nonatomic,strong) NSString * objByes;
+@property(nonatomic,strong) NSNumber * objByes;
 
-@property(nonatomic,strong) NSString * objLegByes;
+@property(nonatomic,strong) NSNumber * objLegByes;
 
-@property(nonatomic,strong) NSString * objPenalty;
+@property(nonatomic,strong) NSNumber * objPenalty;
 
-@property(nonatomic,strong) NSString * objTotalextras;
+@property(nonatomic,strong) NSNumber * objTotalextras;
 
-@property(nonatomic,strong) NSString * objGrandtotal;
+@property(nonatomic,strong) NSNumber * objGrandtotal;
 
-@property(nonatomic,strong) NSString * objRbw;
+@property(nonatomic,strong) NSNumber * objRbw;
 
 @property(nonatomic,strong) NSString * objPMlinecode;
 
 @property(nonatomic,strong) NSString * objPMlengthcode;
 
-@property(nonatomic,strong) NSString * objPMX1;
+@property(nonatomic,strong) NSNumber * objPMX1;
 
-@property(nonatomic,strong) NSString * objPMY1;
+@property(nonatomic,strong) NSNumber * objPMY1;
 
-@property(nonatomic,strong) NSString * objPMX2;
+@property(nonatomic,strong) NSNumber * objPMX2;
 
-@property(nonatomic,strong) NSString * objPMY2;
+@property(nonatomic,strong) NSNumber * objPMY2;
 
-@property(nonatomic,strong) NSString * objPMX3;
+@property(nonatomic,strong) NSNumber * objPMX3;
 
-@property(nonatomic,strong) NSString * objPMY3;
+@property(nonatomic,strong) NSNumber * objPMY3;
 
 @property(nonatomic,strong) NSString * objWWREGION;
 
-@property(nonatomic,strong) NSString * objWWX1;
+@property(nonatomic,strong) NSNumber * objWWX1;
 
-@property(nonatomic,strong) NSString * objWWY1;
+@property(nonatomic,strong) NSNumber * objWWY1;
 
-@property(nonatomic,strong) NSString * objWWX2;
+@property(nonatomic,strong) NSNumber * objWWX2;
 
-@property(nonatomic,strong) NSString * objWWY2;
+@property(nonatomic,strong) NSNumber * objWWY2;
 
-@property(nonatomic,strong) NSString * objballduration;
+@property(nonatomic,strong) NSNumber * objballduration;
 
-@property(nonatomic,strong) NSString * objIsappeal;
+@property(nonatomic,strong) NSNumber * objIsappeal;
 
-@property(nonatomic,strong) NSString * objIsbeaten;
+@property(nonatomic,strong) NSNumber * objIsbeaten;
 
-@property(nonatomic,strong) NSString * objIsuncomfort;
+@property(nonatomic,strong) NSNumber * objIsuncomfort;
 
-@property(nonatomic,strong) NSString * objIswtb;
+@property(nonatomic,strong) NSNumber * objIswtb;
 
-@property(nonatomic,strong) NSString * objIsreleaseshot;
+@property(nonatomic,strong) NSNumber * objIsreleaseshot;
 
 @property(nonatomic,strong) NSString * objMarkedforedit;
 
