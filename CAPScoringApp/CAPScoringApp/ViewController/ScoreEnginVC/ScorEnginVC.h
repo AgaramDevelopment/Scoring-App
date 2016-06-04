348817869ce8a5cac999a52f115fb4da9e57b6a2
AgaramDevelopment <apple@apples-macbook-pro-2.local>
2016-06-04 21:01:11 +0530

    Merge right and left button operation

--- CAPScoringApp/CAPScoringApp/ViewController/ScoreEnginVC/ScorEnginVC.h
+++ CAPScoringApp/CAPScoringApp/ViewController/ScoreEnginVC/ScorEnginVC.h
@@ -168,20 +168,16 @@
 //@property (weak, nonatomic) IBOutlet UIView *view_spin;
 //@property (weak, nonatomic) IBOutlet UIView *view_fast;
 
-//@property (weak, nonatomic) IBOutlet UIView *view_aggressive;
+@property (weak, nonatomic) IBOutlet UIView *view_aggressive;
 
-//@property (weak, nonatomic) IBOutlet UIView *view_defense;
+@property (weak, nonatomic) IBOutlet UIView *view_defense;
 
 @property (weak, nonatomic) IBOutlet UIView *view_fielding_factor;
 
 //@property (weak, nonatomic) IBOutlet UIView *view_otw;
-//@property (weak, nonatomic) IBOutlet UIView *view_rtw;
-//@property (weak, nonatomic) IBOutlet UIView *view_spin;
-//@property (weak, nonatomic) IBOutlet UIView *view_fast;
-//
-//@property (weak, nonatomic) IBOutlet UIView *view_aggressive;
-//
-//@property (weak, nonatomic) IBOutlet UIView *view_defense;
+@property (weak, nonatomic) IBOutlet UIView *view_rtw;
+@property (weak, nonatomic) IBOutlet UIView *view_spin;
+@property (weak, nonatomic) IBOutlet UIView *view_fast;
 
 
 
