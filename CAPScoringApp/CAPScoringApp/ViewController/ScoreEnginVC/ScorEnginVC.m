b8ade4db540deba5d28b3168f5640ddf75e86d0d
AgaramDevelopment <apple@apples-macbook-pro-2.local>
2016-06-11 15:21:35 +0530

    Added Reference for batsman and bowlers

--- CAPScoringApp/CAPScoringApp/ViewController/ScoreEnginVC/ScorEnginVC.m
+++ CAPScoringApp/CAPScoringApp/ViewController/ScoreEnginVC/ScorEnginVC.m
@@ -4325,4 +4325,12 @@
 }
 
 
+- (IBAction)btn_stricker_names:(id)sender {
+}
+- (IBAction)btn_nonstricker_name:(id)sender {
+}
+- (IBAction)btn_bowler_name:(id)sender {
+}
+- (IBAction)btn_last_bowler_name:(id)sender {
+}
 @end
