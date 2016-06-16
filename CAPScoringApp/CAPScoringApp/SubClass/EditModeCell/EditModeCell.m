ec42b76c67d15beba03d484979d82d4ab2a8898a
RamaSubramanian <Ramasubramanian@ramasubramanians-macbook-pro.local>
2016-06-16 18:33:35 +0530

    Add overorder

--- CAPScoringApp/CAPScoringApp/SubClass/EditModeCell/EditModeCell.m
+++ CAPScoringApp/CAPScoringApp/SubClass/EditModeCell/EditModeCell.m
@@ -15,6 +15,13 @@
     [super awakeFromNib];
     // Initialization code
 }
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+    }
+    return self;
+}
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
