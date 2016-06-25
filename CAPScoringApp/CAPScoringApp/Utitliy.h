f748bb68adf455039354a230c679a9af53e6a15a
AgaramDevelopment <apple@apples-macbook-pro-2.local>
2016-06-25 12:21:26 +0530

    Added Common ip post url

---
+++ CAPScoringApp/CAPScoringApp/Utitliy.h
@@ -0,0 +1,13 @@
+//
+//  Utitliy.h
+//  CAPScoringApp
+//
+//  Created by APPLE on 25/06/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utitliy : NSObject
++(NSString *) getIPPORT;
+@end
---
+++ CAPScoringApp/CAPScoringApp/Utitliy.m
@@ -0,0 +1,17 @@
+//
+//  Utitliy.m
+//  CAPScoringApp
+//
+//  Created by APPLE on 25/06/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import "Utitliy.h"
+
+@implementation Utitliy
+
++(NSString *)getIPPORT{
+    return  @"192.168.1.49:8079";
+}
+
+@end
