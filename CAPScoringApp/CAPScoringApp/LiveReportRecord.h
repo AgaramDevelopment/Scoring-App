919072dc186c819a2286fa1c0545c36fd28c122a
Lexicon <user@lexicons-macbook-pro.local>
2016-10-18 14:50:51 +0530

    Added Live DB and Record

---
+++ CAPScoringApp/CAPScoringApp/LiveReportRecord.h
@@ -0,0 +1,30 @@
+//
+//  LiveReportRecord.h
+//  CAPScoringApp
+//
+//  Created by Lexicon on 18/10/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LiveReportRecord : NSObject
+@property(strong,nonatomic)NSString *matchCode;
+@property(strong,nonatomic)NSString *matchName;
+@property(strong,nonatomic)NSString *competitionCode;
+@property(strong,nonatomic)NSString *matchOverComments;
+@property(strong,nonatomic)NSString *matchDate;
+@property(strong,nonatomic)NSString *groundCode;
+@property(strong,nonatomic)NSString *groundName;
+@property(strong,nonatomic)NSString *city;
+@property(strong,nonatomic)NSString *teamAcode;
+@property(strong,nonatomic)NSString *teamBcode;
+@property(strong,nonatomic)NSString *teamAname;
+@property(strong,nonatomic)NSString *teamBname;
+@property(strong,nonatomic)NSString *teamAshortName;
+@property(strong,nonatomic)NSString *teamBshortName;
+@property(strong,nonatomic)NSString *matchOvers;
+@property(strong,nonatomic)NSString *matchTypeName;
+@property(strong,nonatomic)NSString *matchTypeCode;
+@property(strong,nonatomic)NSString *matchStatus;
+@end
---
+++ CAPScoringApp/CAPScoringApp/LiveReportRecord.m
@@ -0,0 +1,33 @@
+//
+//  LiveReportRecord.m
+//  CAPScoringApp
+//
+//  Created by Lexicon on 18/10/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import "LiveReportRecord.h"
+
+@implementation LiveReportRecord
+
+@synthesize matchCode;
+@synthesize matchName;
+@synthesize competitionCode;
+@synthesize matchOverComments;
+@synthesize matchDate;
+@synthesize groundCode;
+@synthesize groundName;
+@synthesize city;
+@synthesize teamAcode;
+@synthesize teamBcode;
+@synthesize teamAname;
+@synthesize teamBname;
+@synthesize teamAshortName;
+@synthesize teamBshortName;
+@synthesize matchOvers;
+@synthesize matchTypeName;
+@synthesize matchTypeCode;
+@synthesize matchStatus;
+
+
+@end
