96e29b4c77c2a942fed085e2629a1d3350da8c0e
Raja sssss <rajasssss@rajas-macbook-pro.local>
2016-10-18 10:47:05 +0530

    added new fixture and results

---
+++ CAPScoringApp/CAPScoringApp/FixtureAndResultsVC.m
@@ -0,0 +1,37 @@
+//
+//  FixtureAndResultsVC.m
+//  CAPScoringApp
+//
+//  Created by Raja sssss on 17/10/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import "FixtureAndResultsVC.h"
+
+@interface FixtureAndResultsVC ()
+
+@end
+
+@implementation FixtureAndResultsVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ CAPScoringApp/CAPScoringApp/FixtureTVC.m
@@ -0,0 +1,24 @@
+//
+//  FixtureAndResultsTVC.m
+//  CAPScoringApp
+//
+//  Created by Raja sssss on 17/10/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import "FixtureTVC.h"
+
+@implementation FixtureAndResultsTVC
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
