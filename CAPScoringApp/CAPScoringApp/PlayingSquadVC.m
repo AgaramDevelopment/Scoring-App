df2000c4e25436777f7dc9908035d012fc7431f8
Raja sssss <rajasssss@rajas-macbook-pro.local>
2016-10-20 18:45:01 +0530

    added icons to assets, changed playing squad , fixture and results

--- CAPScoringApp/CAPScoringApp/PlayingSquadVC.m
+++ CAPScoringApp/CAPScoringApp/PlayingSquadVC.m
@@ -20,11 +20,23 @@
     DBManagerReports *objDBManagerReports;
 }
 @property (nonatomic,strong) NSMutableArray *playerArray;
+@property (nonatomic,strong) NSMutableArray *teamBplayerArray;
 
 @end
 
 @implementation PlayingSquadVC
-@synthesize  matchCode;
+@synthesize matchCode;
+@synthesize teamAname;
+@synthesize teamBname;
+@synthesize venu;
+@synthesize city;
+@synthesize date;
+@synthesize month;
+@synthesize year;
+@synthesize teamACode;
+@synthesize teamBCode;
+@synthesize matchType;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -33,9 +45,18 @@
     objDBManagerReports = [[DBManagerReports alloc]init];
     
 
-    _playerArray =[objDBManagerReports fetchPlayers: self.matchCode];
+    _playerArray =[objDBManagerReports fetchPlayers: self.matchCode : teamACode];
+    
+    _teamBplayerArray = [objDBManagerReports fetchPlayers: self.matchCode : teamBCode];
+    
     
     [self customnavigationmethod];
+    [self fetchMatchdetails];
+    [self setImage:self.teamACode :_img_teamA];
+    [self setImage:self.teamBCode :_img_teamB];
+    
+    self.tbl_players.separatorColor = [UIColor clearColor];
+    
 }
 
 -(void)customnavigationmethod
@@ -49,6 +70,38 @@
     
 }
 
+-(void) setImage:(NSString *)teamCode :(UIImageView *)teamLogoImg {
+    
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *pngFilePath = [NSString stringWithFormat:@"%@/%@.png", docDir,teamCode];
+    
+    
+    BOOL isFileExist = [fileManager fileExistsAtPath:pngFilePath];
+    UIImage *img;
+    if(isFileExist){
+        img = [UIImage imageWithContentsOfFile:pngFilePath];
+        teamLogoImg.image = img;
+    }else{
+        img  = [UIImage imageNamed: @"no_image.png"];
+        teamLogoImg.image = img;
+    }
+}
+
+
+-(void)fetchMatchdetails{
+    
+    
+    self.txt_hme_name.text = [NSString stringWithFormat:@"%@ in %@ - %@",teamAname,teamBname,matchType];
+    self.txt_teamA.text = teamAname;
+    self.txt_teamB.text = teamBname;
+    self.txt_date_venu.text = [NSString stringWithFormat:@"%@ %@ %@ at %@,%@",date,month,year,venu,city];
+    self.txt_teamA_heading.text = teamAname;
+    self.txt_teamB_heading.text = teamBname;
+ 
+    
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
@@ -58,7 +111,10 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return [_playerArray count];    //count number of row from counting array hear cataGorry is An Array
+    return [_playerArray count];
+    return [_teamBplayerArray count];
+    
+    //count number of row from counting array hear cataGorry is An Array
 }
 
 
@@ -66,25 +122,74 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *playingSquad = @"PlayingSquadCell";
     
-
-        PlayingSquadCell *cell = (PlayingSquadCell *)[tableView dequeueReusableCellWithIdentifier:playingSquad];
-        if (cell == nil)
-        {
-            [[NSBundle mainBundle] loadNibNamed:@"PlayingSquadCell" owner:self options:nil];
-            cell = self.playingSquadCell;
-           
-        }
+    
+    
+    static NSString *playingSquad = @"playingSquadCell";
+    
+    PlayingSquadCell *cell = [tableView dequeueReusableCellWithIdentifier:playingSquad
+                                                             forIndexPath:indexPath];
+    
         [cell setBackgroundColor:[UIColor clearColor]];
-        //tableView.allowsSelection = NO;
+  
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
         
         PlayingSquadRecords *record = [_playerArray objectAtIndex:indexPath.row];
+        PlayingSquadRecords *recordB = [_teamBplayerArray objectAtIndex:indexPath.row];
+    
+    
+    if ([_teamBplayerArray count]>indexPath.row) {
         
-        cell.txt_teamA_players.text = record.playerName;
-        //cell.txt_teamB_players.text = record.playerName;
         
+    }
+    
+    cell.txt_teamA_players.text = record.playerName;
+    cell.txt_teamB_players.text = recordB.playerName;
+    
+    //set icon for player roles - team A
+    
+    if ([record.playerRole isEqualToString:@"All Rounder"]) {
+    
+        cell.img_teamA.image = [UIImage imageNamed:@"ico_allrounder.png"];
+        
+        
+    }
+    
+    if ([record.playerRole isEqualToString:@"Bowler"]) {
+        
+        cell.img_teamA.image = [UIImage imageNamed:@"ico_bowler.png"];
+        
+        
+    }
+    
+    if ([record.playerRole isEqualToString:@"Batsman"]) {
+        
+        cell.img_teamA.image = [UIImage imageNamed:@"ico_batsmen.png"];
+        
+    }
+    
+    
+    
+      //set icon for player roles - team B
+    if ([recordB.playerRole isEqualToString:@"All Rounder"]) {
+        
+        cell.img_teamB.image = [UIImage imageNamed:@"ico_allrounder.png"];
+        
+    }
+    
+    if ([recordB.playerRole isEqualToString:@"Bowler"]) {
+        
+       
+        cell.img_teamB.image = [UIImage imageNamed:@"ico_bowler.png"];
+        
+    }
+    
+    if ([recordB.playerRole isEqualToString:@"Batsman"]) {
+        
+       
+        cell.img_teamB.image = [UIImage imageNamed:@"ico_batsmen.png"];
+    }
+    
     
     
         //[self setImage:record.teamCode :cell.img_teamA];
@@ -100,7 +205,7 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
-    return 160;
+    return 52;
     
 }
 
