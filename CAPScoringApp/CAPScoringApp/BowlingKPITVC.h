ae27c0fbe68fd25c0c42931e308552c62c81b384
Raja sssss <rajasssss@rajas-macbook-pro.local>
2016-11-08 18:39:05 +0530

    completed sector wagon report

---
+++ CAPScoringApp/CAPScoringApp/BowlingKPITVC.h
@@ -0,0 +1,13 @@
+//
+//  BowlingKPITVC.h
+//  CAPScoringApp
+//
+//  Created by Raja sssss on 08/11/16.
+//  Copyright © 2016 agaram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BowlingKPITVC : UITableViewCell
+
+@end
